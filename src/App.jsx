7e6b195af3ad8ca3cfd8832f416fb6d32626ee86
Kaninika Sen <kaninikasc@gmail.com>
2024-04-07 20:37:58 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 import { TextOne, TextTwo } from "./components/Texts";
 import TimeLine from "./components/TimeLine";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Hero />
       <TimeLine />
       <TextTwo />
+      <Footer />
     </>
   );
 }
